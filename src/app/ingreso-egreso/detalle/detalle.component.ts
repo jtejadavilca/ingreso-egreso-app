641b9778bb3a07f84ab80a8d56ba1a395ecb8ee8
Jose Tejada <jtejadavilca@gmail.com>
2019-06-20 21:30:19 -0500

    Se quita service alert

--- src/app/ingreso-egreso/detalle/detalle.component.ts
+++ src/app/ingreso-egreso/detalle/detalle.component.ts
@@ -6,7 +6,6 @@ import { Subscription } from 'rxjs';
 import { IngresoEgresoService } from '../ingreso-egreso.service';
 import Swal from 'sweetalert2';
 import { LoadingService } from '../../shared/services/loading.service';
-import { AlertService } from '../../shared/services/alert.service';
 
 @Component({
   selector: 'app-detalle',
@@ -16,37 +15,57 @@ import { AlertService } from '../../shared/services/alert.service';
 export class DetalleComponent implements OnInit, OnDestroy {
 
   items: IngresoEgreso[] = [];
-  subscription: Subscription = new Subscription();
+  cargando: boolean;
+  ingresoEgresoSubs: Subscription = new Subscription();
+  loadingSubs: Subscription = new Subscription();
   constructor(private ingresoEgresoService: IngresoEgresoService,
               private loadingService: LoadingService,
-              private alertService: AlertService,
               private store: Store<AppState>) { }
 
   ngOnInit() {
-    this.subscription = this.store.select('ingresoEgreso')
+    this.loadingSubs  = this.store.select('ui')
+                            .subscribe( ui => this.cargando = ui.isLoading );
+    this.ingresoEgresoSubs = this.store.select('ingresoEgreso')
               .subscribe( ingresoEgreso => {
                 console.log('ingresoEgreso.items', ingresoEgreso.items);
                 this.items = ingresoEgreso.items;
               });
   }
   ngOnDestroy() {
-    this.subscription.unsubscribe();
+    this.ingresoEgresoSubs.unsubscribe();
+    this.loadingSubs.unsubscribe();
   }
 
   pedirConfirmacionEliminar(item: IngresoEgreso) {
-    this.alertService.showConfirmDelete(this.borrarItem, item);
+    console.log(this.loadingService);
+    Swal.fire({
+      title: `Confirmar: <br>Eliminar "${item.descripcion}"`,
+      text: '¿Seguro que desea eliminar el item seleccionado? No se podrá recuperar!',
+      type: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Sí!',
+      cancelButtonText: 'NO!'
+    }).then((result) => {
+      if (result.value) {
+        this.borrarItem(item);
+      }
+    });
   }
 
   borrarItem(item: IngresoEgreso) {
     console.log('item', item);
-    // this.loadingService.initLoading();
-    // this.ingresoEgresoService.borrarIngresoEgreso(item.uid)
-    //     .then( () =>{
-    //       Swal.fire('Item eliminado', 'Se eliminó correctamente el elemento', 'success');
-    //     }).catch( error => {
-    //       Swal.fire('Error', 'Error al intentar eliminar el elemento', 'error');
-    //     }).finally( () => {
-    //       this.loadingService.finishLoading();
-    //     });
+    console.log('this.loadingService', this.loadingService);
+    this.loadingService.initLoading();
+    this.ingresoEgresoService.borrarIngresoEgreso(item.uid)
+        .then( () => {
+          Swal.fire('Item eliminado', `Se eliminó correctamente el elemento <strong>${item.descripcion}</strong>`, 'success');
+        }).catch( error => {
+          Swal.fire('Error', 'Error al intentar eliminar el elemento', 'error');
+          console.log('Error', error);
+        }).finally( () => {
+          this.loadingService.finishLoading();
+        });
   }
 }
