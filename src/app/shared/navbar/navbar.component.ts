d8f931e58cbae7c3e167dd1fe7956fc7a9908003
Jose Tejada <jtejadavilca@gmail.com>
2019-06-20 23:45:49 -0500

    Se setea nombre de usuario en navbar y sidebar

--- src/app/shared/navbar/navbar.component.ts
+++ src/app/shared/navbar/navbar.component.ts
@@ -1,15 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { AppState } from 'src/app/app.reducer';
+import { Subscription } from 'rxjs';
+import { User } from '../../auth/user.model';
+import { AuthState } from '../../auth/auth.reducer';
+import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styles: []
 })
-export class NavbarComponent implements OnInit {
+export class NavbarComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  nombreUsuario: string;
+
+  subscription: Subscription = new Subscription();
+  constructor( private store: Store<AppState> ) { }
 
   ngOnInit() {
+    this.subscription = this.store.select('auth')
+    .pipe(
+      filter( auth => auth.user != null )
+    ).subscribe( (auth: AuthState) => {
+      console.log('auth', auth);
+      this.nombreUsuario = auth.user.nombre;
+    });
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
   }
 
 }
--- src/app/shared/sidebar/sidebar.component.ts
+++ src/app/shared/sidebar/sidebar.component.ts
@@ -1,16 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { AuthService } from '../../auth/auth.service';
+import { Subscription } from 'rxjs';
+import { Store } from '@ngrx/store';
+import { AppState } from 'src/app/app.reducer';
+import { AuthState } from '../../auth/auth.reducer';
+import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
   styles: []
 })
-export class SidebarComponent implements OnInit {
+export class SidebarComponent implements OnInit, OnDestroy {
+  nombreUsuario: string;
 
-  constructor(public authService: AuthService) { }
+  subscription: Subscription = new Subscription();
+  constructor(public authService: AuthService,
+              private store: Store<AppState>) { }
 
   ngOnInit() {
+    this.subscription = this.store.select('auth')
+    .pipe(
+      filter( auth => auth.user != null )
+    ).subscribe( (auth: AuthState) => {
+      console.log('auth', auth);
+      this.nombreUsuario = auth.user.nombre;
+    });
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
   }
 
   cerrarSesion() {
