feb27ec28db88c3e933a20601b5a066d513665af
Jose Tejada <jtejadavilca@gmail.com>
2019-06-20 22:14:37 -0500

    Avance de componente estadísticas

--- src/app/ingreso-egreso/estadistica/estadistica.component.ts
+++ src/app/ingreso-egreso/estadistica/estadistica.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { AppState } from 'src/app/app.reducer';
+import { Store } from '@ngrx/store';
+import { Subscription } from 'rxjs';
+import { IngresoEgreso } from '../ingreso-egreso.model';
 
 @Component({
   selector: 'app-estadistica',
@@ -7,9 +11,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EstadisticaComponent implements OnInit {
 
-  constructor() { }
+  ingresos: number;
+  egresos: number;
+
+  cuantosIngresos: number;
+  cuantosEgresos: number;
+
+  subscription: Subscription = new Subscription();
+
+  constructor( private store: Store<AppState> ) { }
 
   ngOnInit() {
+    this.subscription = this.store.select('ingresoEgreso')
+            .subscribe( ingresoEgreso => {
+              this.contarIngresosEgresos( ingresoEgreso.items );
+            });
   }
 
+  contarIngresosEgresos( items: IngresoEgreso[] ) {
+    this.ingresos = 0;
+    this.egresos = 0;
+
+    this.cuantosIngresos = 0;
+    this.cuantosEgresos = 0;
+
+    items.forEach( item => {
+      if ( item.tipo === 'ingreso' ) {
+        this.cuantosIngresos++;
+        this.ingresos += item.monto;
+      } else {
+        this.cuantosEgresos++;
+        this.egresos += item.monto;
+      }
+    });
+  }
 }
